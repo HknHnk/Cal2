d9be34f50897f5ada4b6f01c613c0862a5636ebc
HknHnk <joshuakk008@gmail.com>
2025-02-18 20:25:15 +0000

    Update AddItemModal.tsx

--- Components/AddItemModal.tsx
+++ Components/AddItemModal.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './AddItemModal.css'; // Import the CSS file for the modal
 
 interface AddItemModalProps {
   showModal: boolean;
@@ -10,6 +11,7 @@ interface AddItemModalProps {
   endTime: string;
   setEndTime: (time: string) => void;
   handleAddItem: () => void;
+  handleKeyPress: (event: React.KeyboardEvent<HTMLInputElement>) => void;
 }
 
 const AddItemModal: React.FC<AddItemModalProps> = ({
@@ -22,33 +24,37 @@ const AddItemModal: React.FC<AddItemModalProps> = ({
   endTime,
   setEndTime,
   handleAddItem,
+  handleKeyPress,
 }) => {
-  if (!showModal) return null;
+  if (!showModal) {
+    return null;
+  }
 
   return (
     <div className="modal">
       <div className="modal-content">
-        <button className="modal-close" onClick={closeModal}>×</button>
-        <h3>Add Item</h3>
+        <button className="modal-close" onClick={closeModal}>X</button>
+        <h3>Add New Item</h3>
         <input
           type="text"
-          placeholder="Content"
+          placeholder="Item content"
           value={newItemContent}
           onChange={(e) => setNewItemContent(e.target.value)}
+          onKeyPress={handleKeyPress}
         />
         <input
           type="time"
-          placeholder="Start Time"
+          placeholder="Start time"
           value={startTime}
           onChange={(e) => setStartTime(e.target.value)}
         />
         <input
           type="time"
-          placeholder="End Time"
+          placeholder="End time"
           value={endTime}
           onChange={(e) => setEndTime(e.target.value)}
         />
-        <button onClick={handleAddItem}>Add</button>
+        <button onClick={handleAddItem}>Add Item</button>
         <button onClick={closeModal}>Cancel</button>
       </div>
     </div>
